557da5050eca1d47914305e90388cc8a874ff835
Sayantan Bhanjo Chowdhury <sayantan.bhanjochowdhury@gmail.com>
2026-08-12 05:43:55 +0530

    feat: add SEO meta tags and react-helmet-async

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,2 @@
-import {StrictMode} from 'react';import {createRoot} from 'react-dom/client';import './index.css';import App from './App.tsx';createRoot(document.getElementById('root')!).render(<StrictMode><App/></StrictMode>);
+import {StrictMode} from 'react';import {createRoot} from 'react-dom/client';import './index.css';
+import App from './App.tsx';createRoot(document.getElementById('root')!).render(<StrictMode><App/></StrictMode>);
